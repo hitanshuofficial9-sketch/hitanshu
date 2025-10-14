0bcda5f170f556185c8d7081b99a0c2fe7bf47d5
hitanshu <hitanshuiofficial9@gmail.com>
2025-10-14 12:32:23 +0530

    startswith , endswith ,find string function

---
+++ 01_startwith.py
@@ -0,0 +1,6 @@
+# a="hello world"
+str = "Hello World"
+print(str.startswith("Hel"))
+print(str.startswith("he"))
+print(str.startswith("H"))
+print(str.startswith("Wo"))
---
+++ 02_ends_with.py
@@ -0,0 +1,5 @@
+str = "Hello World"
+print(str.endswith("Hel"))
+print(str.endswith("d"))
+print(str.endswith("ld"))
+print(str.endswith("Wo"))
